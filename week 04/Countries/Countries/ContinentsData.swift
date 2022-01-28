041e6bf3bd133e137ed270813f9180df888d0ed2
Aileen Pierce <aileen.pierce@colorado.edu>
2022-01-28 09:37:03 -0700

    navigation controllers

---
+++ week 04/Countries/Countries/ContinentsData.swift
@@ -0,0 +1,14 @@
+//
+//  ContinentsData.swift
+//  Countries
+//
+//  Created by Aileen Pierce
+//
+
+import Foundation
+
+struct ContinentsData : Codable {
+    var continent : String
+    var countries : [String]
+}
+
