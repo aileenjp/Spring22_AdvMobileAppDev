869d088fd945baddd5e5fda16228303b1de28320
Aileen Pierce <aileen.pierce@colorado.edu>
2022-04-13 14:33:51 -0600

    realm

---
+++ week 14/List Realm/app/src/main/java/com/example/list/model/Item.kt
@@ -0,0 +1,8 @@
+package com.example.list.model
+
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+
+open class Item(@PrimaryKey var id: String, var name: String): RealmObject(){
+    constructor(): this("", "")
+}
